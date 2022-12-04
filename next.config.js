0f6a40589e4c1036d1b346f40d3361adf811d55a
limcpf <matathresh1740@naver.com>
2022-12-04 23:15:05 +0900

    remove: testPost.ts and webpack config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  webpack: config => {
-    // 아래를 추가합니다.
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"]
-    });
-    return config;
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig
