0d0e19d24f166d9b63469192f43812b3684ffa83
limcpf <matathresh1740@naver.com>
2022-12-28 23:43:49 +0900

    feat: add code template

--- shared/post/markdown.ts
+++ shared/post/markdown.ts
@@ -5,12 +5,31 @@ const md = markdown({
 	xhtmlOut: false,
 	breaks: false,
 	langPrefix: "language-",
-	linkify: false,
-	typographer: false,
+	linkify: true,
+	typographer: true,
 	quotes: "“”‘’",
 	highlight: function (/*str, lang*/) {
 		return "";
-	},
+	}
 });
+
+md.renderer.rules.fence = function (tokens, idx, options, env, self) {
+	const token = tokens[idx];
+
+	let contents = tokens[idx].content;
+
+	const titleStartIdx = contents.indexOf("^^^");
+
+	if(titleStartIdx !== -1) {
+		const titleContentsStartIdx = titleStartIdx + 3;
+		const titleEndIdx = contents.indexOf("^^^", titleContentsStartIdx);
+		const good = '<div class="code-top">' + contents.substring(titleContentsStartIdx, titleEndIdx) + '</div>';
+		 contents= good + '<code>' + contents.substring(titleEndIdx+4) + '</code>';
+	} else {
+		 contents= '<div class="code-top">example</div><code>' + contents + '</code>'
+	}
+
+	return '<pre' + self.renderAttrs(token) + '>' + contents + '</pre>';
+}
 
 export default md;
